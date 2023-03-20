5ebe9cceaa07649ecb00d4ac0ce71549f3e630a3
Արսեն Գաբրիելյան / Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2023-03-20 17:09:22 +0400

    Added Autocomplete Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  users: string[] = [
+    "Arsen Gabrielyan",
+    "Gev Gabrielyan",
+    "John Addams",
+    "Tom Smith",
+    "Leanne Graham",
+    "Ervin Howell",
+    "Clementine Bauch",
+    "Patricia Lebsack",
+    "Chelsey Dietrich",
+    "Kurtis Weissnat",
+    "Glenna Reichert",
+    "Clementina DuBuque",
+    "Petros Poghosyan",
+    "Poghos Petrosyan"
+  ]
 }
