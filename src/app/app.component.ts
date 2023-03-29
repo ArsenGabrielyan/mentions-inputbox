b8ac18eef14c3ff093f6209335b416be971e3658
Արսեն Գաբրիելյան / Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2023-03-29 17:06:12 +0400

    Added Actions to Users List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IUserList } from './interfaces/userlist';
 
 @Component({
   selector: 'app-root',
@@ -6,21 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  users: string[] = [
-    "Arsen Gabrielyan",
-    "Gev Gabrielyan",
-    "John Addams",
-    "Tom Smith",
-    "Leanne Graham",
-    "Ervin Howell",
-    "Clementine Bauch",
-    "Patricia Lebsack",
-    "Chelsey Dietrich",
-    "Kurtis Weissnat",
-    "Glenna Reichert",
-    "Clementina DuBuque",
-    "Petros Poghosyan",
-    "Poghos Petrosyan"
+  users: IUserList[] = [
+    {name: "Arsen Gabrielyan",action: ()=>{this.txtInput=this.users[0].name;this.mentioned=false;}},
+    {name: "Gev Gabrielyan",action: ()=>{this.txtInput=this.users[1].name;this.mentioned=false;}},
+    {name: "John Addams",action: ()=>{this.txtInput=this.users[2].name;this.mentioned=false;}},
+    {name: "Tom Smith",action: ()=>{this.txtInput=this.users[3].name;this.mentioned=false;}},
+    {name: "Leanne Graham",action: ()=>{this.txtInput=this.users[4].name;this.mentioned=false;}},
+    {name: "Ervin Howell",action: ()=>{this.txtInput=this.users[5].name;this.mentioned=false;}},
+    {name: "Clementine Bauch",action: ()=>{this.txtInput=this.users[6].name;this.mentioned=false;}},
+    {name: "Patricia Lebsack",action: ()=>{this.txtInput=this.users[7].name;this.mentioned=false;}},
+    {name: "Chelsey Dietrich",action: ()=>{this.txtInput=this.users[8].name;this.mentioned=false;}},
+    {name: "Kurtis Weissnat",action: ()=>{this.txtInput=this.users[9].name;this.mentioned=false;}},
+    {name: "Glenna Reichert",action: ()=>{this.txtInput=this.users[10].name;this.mentioned=false;}},
+    {name: "Clementina DuBuque",action: ()=>{this.txtInput=this.users[11].name;this.mentioned=false;}},
+    {name: "Petros Poghosyan",action: ()=>{this.txtInput=this.users[12].name;this.mentioned=false;}},
+    {name: "Poghos Petrosyan",action: ()=>{this.txtInput=this.users[13].name;this.mentioned=false;}},
+    {name: "Armen Gabrielyan",action: ()=>{this.txtInput=this.users[14].name;this.mentioned=false;}},
   ];
   txtInput = "";
   mentioned = false;
--- src/app/pipes/filter-user.pipe.ts
+++ src/app/pipes/filter-user.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { IUserList } from '../interfaces/userlist';
 
 @Pipe({
   name: 'filterUser'
 })
 export class FilterUserPipe implements PipeTransform {
-  transform(value: string[], query: string): string[] {
+  transform(value: IUserList[], query: string): IUserList[] {
     if(!query) return value;
-    return [...value.filter(val=>val.toLowerCase().includes(query))];
+    return [...value.filter(val=>val.name.toLowerCase().includes(query.toLowerCase()))];
   }
 }
